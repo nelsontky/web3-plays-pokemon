89f65098fd4edc4d6a6f4d351c8b4ddacc73baa2
nelsontky <nelson@ccb.wtf>
2022-12-29 13:31:21 +0800

    feat: allow voting in contract

--- apps/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/lib.rs
+++ apps/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/lib.rs
@@ -13,7 +13,7 @@ pub mod solana_plays_pokemon_program {
         msg!("Game data account initialized");
 
         let game_state = &mut ctx.accounts.game_state;
-        game_state.is_voting_closed = true;
+        game_state.has_executed = true;
         game_state.second = 0;
 
         game_state.up_count = 0;
@@ -30,6 +30,23 @@ pub mod solana_plays_pokemon_program {
         game_state.save_state_cid = save_state_cid;
         msg!("FIrst game state account initialized");
 
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, joypad_button: JoypadButton) -> Result<()> {
+        let current_game_state = &mut ctx.accounts.current_game_state;
+        match joypad_button {
+            JoypadButton::Up => current_game_state.up_count = current_game_state.up_count.checked_add(1).unwrap(),
+            JoypadButton::Down => current_game_state.down_count = current_game_state.down_count.checked_add(1).unwrap(),
+            JoypadButton::Left => current_game_state.left_count = current_game_state.left_count.checked_add(1).unwrap(),
+            JoypadButton::Right => current_game_state.right_count = current_game_state.right_count.checked_add(1).unwrap(),
+            JoypadButton::A => current_game_state.a_count = current_game_state.a_count.checked_add(1).unwrap(),
+            JoypadButton::B => current_game_state.b_count = current_game_state.b_count.checked_add(1).unwrap(),
+            JoypadButton::Start => current_game_state.start_count = current_game_state.start_count.checked_add(1).unwrap(),
+            JoypadButton::Select => current_game_state.select_count = current_game_state.select_count.checked_add(1).unwrap(),
+            JoypadButton::Nothing => current_game_state.nothing_count = current_game_state.nothing_count.checked_add(1).unwrap(),
+        }
+        
         Ok(())
     }
 }
@@ -52,6 +69,27 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Vote<'info> {
+    #[account(
+        init_if_needed, 
+        payer = player, 
+        space = 8 + 1 + (10 * 8) + 4 + 59 + 4 + 59, // nft.storage cids are 59 characters long by default
+        seeds = [
+                game_data.key().as_ref(), 
+                b"game_state", 
+                game_data.seconds_played.to_string().as_ref()
+            ],
+        bump
+    )]
+    current_game_state: Account<'info, GameState>,
+    #[account()]
+    pub game_data: Account<'info, GameData>,
+    #[account(mut)]
+    pub player: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct GameData {
     pub seconds_played: u32,
@@ -64,7 +102,7 @@ impl GameData {
 
 #[account]
 pub struct GameState {
-    pub is_voting_closed: bool,
+    pub has_executed: bool,
     pub second: u32,
 
     pub up_count: u32,
@@ -79,4 +117,17 @@ pub struct GameState {
 
     pub frames_image_cid: String,
     pub save_state_cid: String,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub enum JoypadButton {
+    Up,
+    Down,
+    Left,
+    Right,
+    A,
+    B,
+    Start,
+    Select,
+    Nothing,
 }
--- apps/solana-plays-pokemon-program/tests/solana-plays-pokemon-program.ts
+++ apps/solana-plays-pokemon-program/tests/solana-plays-pokemon-program.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { AnchorError, Program } from "@project-serum/anchor";
 import { assert } from "chai";
 import { SolanaPlaysPokemonProgram } from "../target/types/solana_plays_pokemon_program";
 
@@ -50,12 +50,11 @@ describe("solana-plays-pokemon-program", () => {
     const gameStateAccount = await program.account.gameState.fetch(
       gameStatePda
     );
-    assert.strictEqual(gameStateAccount.isVotingClosed, true);
+    assert.strictEqual(gameStateAccount.hasExecuted, true);
     assert.strictEqual(gameStateAccount.second, 0);
 
     assert.strictEqual(gameStateAccount.upCount, 0);
     assert.strictEqual(gameStateAccount.downCount, 0);
-    assert.strictEqual(gameStateAccount.leftCount, 0);
     assert.strictEqual(gameStateAccount.leftCount, 0);
     assert.strictEqual(gameStateAccount.rightCount, 0);
     assert.strictEqual(gameStateAccount.aCount, 0);
@@ -66,5 +65,106 @@ describe("solana-plays-pokemon-program", () => {
 
     assert.strictEqual(gameStateAccount.framesImageCid, FRAMES_IMAGES_CID);
     assert.strictEqual(gameStateAccount.saveStateCid, SAVE_STATE_CID);
+  });
+
+  it("Cannot vote to game state account with invalid game data account", async () => {
+    const gameDataAccount = await program.account.gameData.fetch(
+      gameData.publicKey
+    );
+    const invalidGameData = anchor.web3.Keypair.generate();
+    const secondsPlayed = gameDataAccount.secondsPlayed;
+    const [invalidGameStatePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        invalidGameData.publicKey.toBuffer(),
+        Buffer.from("game_state"),
+        Buffer.from("" + secondsPlayed),
+      ],
+      program.programId
+    );
+
+    try {
+      await program.methods
+        .vote({ a: {} })
+        .accounts({
+          currentGameState: invalidGameStatePda,
+          gameData: gameData.publicKey,
+          player: anchor.getProvider().publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+    } catch (e) {
+      assert.instanceOf(e, AnchorError);
+    }
+  });
+
+  it("Cannot vote to game state account with invalid seconds played count", async () => {
+    const gameDataAccount = await program.account.gameData.fetch(
+      gameData.publicKey
+    );
+    const secondsPlayed = gameDataAccount.secondsPlayed;
+    const [invalidGameStatePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        gameData.publicKey.toBuffer(),
+        Buffer.from("game_state"),
+        Buffer.from("" + (secondsPlayed - 1)),
+      ],
+      program.programId
+    );
+
+    try {
+      await program.methods
+        .vote({ a: {} })
+        .accounts({
+          currentGameState: invalidGameStatePda,
+          gameData: gameData.publicKey,
+          player: anchor.getProvider().publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+    } catch (e) {
+      assert.instanceOf(e, AnchorError);
+    }
+  });
+
+  it("Can vote for a move", async () => {
+    const gameDataAccount = await program.account.gameData.fetch(
+      gameData.publicKey
+    );
+    const secondsPlayed = gameDataAccount.secondsPlayed;
+    const [gameStatePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        gameData.publicKey.toBuffer(),
+        Buffer.from("game_state"),
+        Buffer.from("" + secondsPlayed),
+      ],
+      program.programId
+    );
+
+    await program.methods
+      .vote({ a: {} })
+      .accounts({
+        currentGameState: gameStatePda,
+        gameData: gameData.publicKey,
+        player: anchor.getProvider().publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    await program.methods
+      .vote({ b: {} })
+      .accounts({
+        currentGameState: gameStatePda,
+        gameData: gameData.publicKey,
+        player: anchor.getProvider().publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    const currentGameState = await program.account.gameState.fetch(
+      gameStatePda
+    );
+
+    assert.strictEqual(currentGameState.aCount, 1);
+    assert.strictEqual(currentGameState.bCount, 1);
   });
 });
