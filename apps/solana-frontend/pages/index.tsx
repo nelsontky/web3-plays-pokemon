a67c07133ccfe516bff6772af20e7adeedb7c476
nelsontky <nelson@ccb.wtf>
2023-01-03 00:53:19 +0800

    feat: add site tags

--- apps/solana-frontend/pages/index.tsx
+++ apps/solana-frontend/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import tw from "twin.macro";
 import Controls from "../components/Controls";
 import CurrentVotes from "../components/CurrentVotes";
@@ -5,9 +6,35 @@ import GameCanvas from "../components/GameCanvas";
 import HowToPlay from "../components/HowToPlay";
 import VotesHistory from "../components/VotesHistory";
 
+const DESCRIPTION = "Play Pokemon collaboratively on the Solana blockchain!";
+
 export default function Web() {
   return (
     <div css={tw`max-w-2xl mx-auto px-4 pb-8`}>
+      <Head>
+        <title>Solana Plays Pokemon</title>
+        <meta name="description" content={DESCRIPTION} key="desc" />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content="/og-image.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
       <GameCanvas />
       <Controls />
       <CurrentVotes />
