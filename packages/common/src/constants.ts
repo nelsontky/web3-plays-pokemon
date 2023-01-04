480cae6a5bc82f7c4119e364126afc89db44308f
nelsontky <nelson@ccb.wtf>
2023-01-04 22:32:30 +0800

    feat: update to mainnet variables

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -4,11 +4,11 @@ export const GAMEBOY_FPS = 60;
 export const FPS = 30;
 
 // Mainnet
-// export const PROGRAM_ID = "pkmNUoVrc8m4DkvQkKDHrffDEPJwVhuXqQv3hegbVyg";
-// export const GAME_DATA_ACCOUNT_ID =
-//   "redHPYj5C9X7TyEqQPHMTsfevKZ9eHCTQ8BJVee9JWC";
+export const PROGRAM_ID = "pkmNUoVrc8m4DkvQkKDHrffDEPJwVhuXqQv3hegbVyg";
+export const GAME_DATA_ACCOUNT_ID =
+  "redHPYj5C9X7TyEqQPHMTsfevKZ9eHCTQ8BJVee9JWC";
 
 // Devnet
-export const PROGRAM_ID = "pkmJNXmUxFT1bmmCp4DgvCm2LxR3afRtCwV1EzQwEHK";
-export const GAME_DATA_ACCOUNT_ID =
-  "red5F8QaCQaPLWTRBBt85Ghm4JrP79TuBcDRCPZtgYh";
+// export const PROGRAM_ID = "pkmJNXmUxFT1bmmCp4DgvCm2LxR3afRtCwV1EzQwEHK";
+// export const GAME_DATA_ACCOUNT_ID =
+//   "red5F8QaCQaPLWTRBBt85Ghm4JrP79TuBcDRCPZtgYh";
