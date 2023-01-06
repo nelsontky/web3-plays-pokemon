c8b9fb95ba2cbb0b579e1f517bb09fa144e8968c
nelsontky <nelson@ccb.wtf>
2023-01-06 16:23:39 +0800

    feat: better error handling in be

--- apps/solana-frontend/pages/api/messages.ts
+++ apps/solana-frontend/pages/api/messages.ts
@@ -9,38 +9,42 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(404).end();
+  try {
+    if (req.method !== "POST") {
+      return res.status(404).end();
+    }
+
+    const { publicKey, text }: { publicKey?: string; text?: string } = req.body;
+    const signature = req.headers.authorization;
+    if (!signature || !publicKey) {
+      return res.status(401).end();
+    }
+
+    const isSignatureValid = nacl.sign.detached.verify(
+      new TextEncoder().encode(SIGNATURE_MESSAGE),
+      Buffer.from(signature, "base64"),
+      Uint8Array.from(new PublicKey(publicKey).toBuffer())
+    );
+
+    if (!isSignatureValid) {
+      return res.status(401).end();
+    }
+
+    const trimmedText = text?.trim();
+    if (!trimmedText || trimmedText.length === 0) {
+      return res.status(400).end();
+    }
+
+    await create({
+      text: trimmedText,
+      timestamp: Date.now(),
+      walletAddress: publicKey,
+    });
+
+    return res.status(200).end();
+  } catch {
+    return res.status(500).end();
   }
-
-  const { publicKey, text }: { publicKey?: string; text?: string } = req.body;
-  const signature = req.headers.authorization;
-  if (!signature || !publicKey) {
-    return res.status(401).end();
-  }
-
-  const isSignatureValid = nacl.sign.detached.verify(
-    new TextEncoder().encode(SIGNATURE_MESSAGE),
-    Buffer.from(signature, "base64"),
-    Uint8Array.from(new PublicKey(publicKey).toBuffer())
-  );
-
-  if (!isSignatureValid) {
-    return res.status(401).end();
-  }
-
-  const trimmedText = text?.trim();
-  if (!trimmedText || trimmedText.length === 0) {
-    return res.status(400).end();
-  }
-
-  await create({
-    text: trimmedText,
-    timestamp: Date.now(),
-    walletAddress: publicKey,
-  });
-
-  return res.status(200).end();
 }
 
 async function create(message: Message) {
