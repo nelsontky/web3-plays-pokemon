acd0ad826482ea0a1b0b45b985f474af0cef5031
nelsontky <nelson@ccb.wtf>
2023-01-09 07:06:38 +0800

    refactor: update dependencies array

--- apps/solana-frontend/hooks/useGameStateListener.tsx
+++ apps/solana-frontend/hooks/useGameStateListener.tsx
@@ -111,6 +111,7 @@ export default function useGameStateListener() {
       program.account.gameState,
       gameStatePdaPrev,
       didGameStatePdaChange,
+      program.account.gameStateV2,
     ]
   );
 }
