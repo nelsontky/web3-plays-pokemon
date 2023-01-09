2ca87e76fad570d6b8c47989e421e3db0f9407e6
nelsontky <nelson@ccb.wtf>
2023-01-09 22:31:45 +0800

    feat: add pokemon memory location

--- packages/common/src/pokemonRomMemoryUtils.ts
+++ packages/common/src/pokemonRomMemoryUtils.ts
@@ -1,3 +1,15 @@
+// https://datacrystal.romhacking.net/wiki/Pok%C3%A9mon_Red/Blue:RAM_map
+
 export const GAMEBOY_MEMORY_OFFSET = 0x8000;
 
 export const LETTER_PRINTING_DELAY_FLAGS_LOCATION = 0xd358;
+
+// Pokemon 1, use as starting point for all pokemon
+export const POKEMON_1_NAME = 0xd16b;
+export const POKEMON_1_HP_START = 0xd16c;
+export const POKEMON_1_LEVEL = 0xd18c;
+export const POKEMON_1_MOVE_1 = 0xd19f;
+
+// Pokemon data offsets
+export const POKEMON_HP_SIZE = 2;
+export const POKEMON_SIZE = 44;
