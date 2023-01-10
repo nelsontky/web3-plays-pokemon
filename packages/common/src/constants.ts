6cf5e2f7606b2791280ee8d30fac9e06c74c7dab
nelsontky <nelson@ccb.wtf>
2023-01-10 17:48:31 +0800

    feat: deploy to mainnet

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -32,11 +32,11 @@ export const BUTTON_ID_TO_ENUM = [
 ];
 
 // Mainnet
-// export const PROGRAM_ID = "pkmNUoVrc8m4DkvQkKDHrffDEPJwVhuXqQv3hegbVyg";
-// export const GAME_DATA_ACCOUNT_ID =
-//   "redHPYj5C9X7TyEqQPHMTsfevKZ9eHCTQ8BJVee9JWC";
+export const PROGRAM_ID = "pkmNUoVrc8m4DkvQkKDHrffDEPJwVhuXqQv3hegbVyg";
+export const GAME_DATA_ACCOUNT_ID =
+  "redHPYj5C9X7TyEqQPHMTsfevKZ9eHCTQ8BJVee9JWC";
 
 // Devnet
-export const PROGRAM_ID = "pkmJNXmUxFT1bmmCp4DgvCm2LxR3afRtCwV1EzQwEHK";
-export const GAME_DATA_ACCOUNT_ID =
-  "redu6eq7Bo6NKqtuK2ubJbYupcMQxqh77FYXmFtsjSH";
+// export const PROGRAM_ID = "pkmJNXmUxFT1bmmCp4DgvCm2LxR3afRtCwV1EzQwEHK";
+// export const GAME_DATA_ACCOUNT_ID =
+//   "redu6eq7Bo6NKqtuK2ubJbYupcMQxqh77FYXmFtsjSH";
