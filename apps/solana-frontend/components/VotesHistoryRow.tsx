28c1b7608dc76a78c201061a9da0a55d1c6f40ff
nelsontky <nelson@ccb.wtf>
2023-01-13 05:26:34 +0800

    fix: remove wrong types

--- apps/solana-frontend/components/VotesHistoryRow.tsx
+++ apps/solana-frontend/components/VotesHistoryRow.tsx
@@ -24,33 +24,34 @@ export default function VotesHistoryRow({ gameStateId }: VotesHistoryRowProps) {
     return null;
   }
 
-  return (
-    <tr>
-      <td css={styles.tableData}>{gameState.index}</td>
-      <td css={styles.tableData}>{gameState.votes[1]}</td>
-      <td css={styles.tableData}>{gameState.votes[2]}</td>
-      <td css={styles.tableData}>{gameState.votes[3]}</td>
-      <td css={styles.tableData}>{gameState.votes[4]}</td>
-      <td css={styles.tableData}>{gameState.votes[5]}</td>
-      <td css={styles.tableData}>{gameState.votes[6]}</td>
-      <td css={styles.tableData}>{gameState.votes[7]}</td>
-      <td css={styles.tableData}>{gameState.votes[8]}</td>
-      <td css={styles.tableData}>{gameState.votes[9]}</td>
-      <td css={styles.tableData}>{gameState.votes[10]}</td>
-      <td css={styles.tableData}>{gameState.votes[13]}</td>
-      <td css={styles.tableData}>{gameState.votes[14]}</td>
-      <td css={styles.tableData}>{gameState.votes[11]}</td>
-      <td css={styles.tableData}>{gameState.votes[12]}</td>
-      <td css={styles.tableData}>{gameState.votes[0]}</td>
-      <td css={styles.tableData}>
-        <SmallControl
-          containerStyles={{
-            transform: "scale(0.6) translateY(-1rem)",
-          }}
-        >
-          {gameState.executedButton}
-        </SmallControl>
-      </td>
-    </tr>
-  );
+  return null;
+  // return (
+  //   <tr>
+  //     <td css={styles.tableData}>{gameState.index}</td>
+  //     <td css={styles.tableData}>{gameState.votes[1]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[2]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[3]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[4]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[5]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[6]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[7]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[8]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[9]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[10]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[13]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[14]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[11]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[12]}</td>
+  //     <td css={styles.tableData}>{gameState.votes[0]}</td>
+  //     <td css={styles.tableData}>
+  //       <SmallControl
+  //         containerStyles={{
+  //           transform: "scale(0.6) translateY(-1rem)",
+  //         }}
+  //       >
+  //         {gameState.executedButton}
+  //       </SmallControl>
+  //     </td>
+  //   </tr>
+  // );
 }
