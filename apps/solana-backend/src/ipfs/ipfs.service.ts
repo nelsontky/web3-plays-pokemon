9302095b83c4ea641a526b517a815df7c08705ae
nelsontky <nelson@ccb.wtf>
2023-01-12 15:57:50 +0800

    feat: setup ipfs http client

--- apps/solana-backend/src/ipfs/ipfs.service.ts
+++ apps/solana-backend/src/ipfs/ipfs.service.ts
@@ -4,36 +4,25 @@ import { Web3Storage } from "web3.storage";
 import axios from "axios";
 import { FsBlockStore } from "ipfs-car/blockstore/fs";
 import { Cron } from "@nestjs/schedule";
+import * as IPFS from "ipfs-http-client";
 
 @Injectable()
 export class IpfsService {
   private readonly logger = new Logger(IpfsService.name);
-  private ipfsProvider: "web3.storage" | "nft.storage";
+  private readonly client: any;
+  private pinningProvider: "web3.storage" | "nft.storage";
 
   constructor() {
-    this.ipfsProvider = "nft.storage";
+    this.pinningProvider = "nft.storage";
+    this.client = IPFS();
   }
 
   async upload(data: Uint8Array) {
-    if (this.ipfsProvider === "nft.storage") {
-      try {
-        const cid = await this.uploadToNftStorage(data);
-        return cid;
-      } catch (e) {
-        this.logger.warn(`nft.storage error: ${e}. Trying web3.storage now...`);
-        this.ipfsProvider = "web3.storage";
-      }
-    }
+    const { cid } = await this.client.add(data, {
+      cidVersion: 1,
+    });
 
-    try {
-      const cid = await this.uploadToWeb3Storage(data);
-      return cid;
-    } catch (e) {
-      this.ipfsProvider = "nft.storage";
-      throw new Error(
-        `web3.storage error: ${e}. Trying nft.storage on next run, fingers crossed...`,
-      );
-    }
+    return cid.toString();
   }
 
   async download(cid: string) {
@@ -46,25 +35,66 @@ export class IpfsService {
 
   @Cron("0 */10 * * * *")
   revertToNftStorage() {
-    if (this.ipfsProvider !== "nft.storage") {
+    if (this.pinningProvider !== "nft.storage") {
       this.logger.log(
         "Automatically switching ipfs service back to nft.storage",
       );
-      this.ipfsProvider = "nft.storage";
+      this.pinningProvider = "nft.storage";
     }
   }
 
-  private uploadToNftStorage(data: Uint8Array) {
+  private async uploadToPinningService(data: Uint8Array) {
+    let cid: string | undefined = undefined;
+
+    while (cid === undefined) {
+      if (this.pinningProvider === "nft.storage") {
+        try {
+          cid = await this.uploadToNftStorage(data);
+          return cid;
+        } catch (e) {
+          this.logger.warn(
+            `nft.storage error: ${e}. Trying web3.storage on next run.`,
+          );
+          this.pinningProvider = "web3.storage";
+        }
+      } else {
+        try {
+          cid = await this.uploadToWeb3Storage(data);
+        } catch (e) {
+          this.pinningProvider = "nft.storage";
+          this.logger.warn(
+            `web3.storage error: ${e}. Trying nft.storage on next run, fingers crossed...`,
+          );
+        }
+      }
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, 500);
+      });
+    }
+
+    return cid;
+  }
+
+  private async uploadToNftStorage(data: Uint8Array) {
     const client = new NFTStorage({ token: process.env.NFT_STORAGE_TOKEN });
-    return client.storeBlob(new Blob([data]));
+    const cid = await client.storeBlob(new Blob([data]));
+    return cid.toString();
   }
 
   private async uploadToWeb3Storage(data: Uint8Array) {
-    const { car } = await NFTStorage.encodeBlob(new Blob([data]), {
-      blockstore: new FsBlockStore(),
-    });
+    const blockstore = new FsBlockStore();
 
-    const client = new Web3Storage({ token: process.env.WEB3_STORAGE_TOKEN });
-    return client.putCar(car);
+    try {
+      const { car } = await NFTStorage.encodeBlob(new Blob([data]), {
+        blockstore,
+      });
+
+      const client = new Web3Storage({ token: process.env.WEB3_STORAGE_TOKEN });
+      const cid = await client.putCar(car);
+      return cid.toString();
+    } finally {
+      await blockstore.close();
+    }
   }
 }
