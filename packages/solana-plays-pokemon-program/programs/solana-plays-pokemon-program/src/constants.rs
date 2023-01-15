f627dd2999c870ab5f170007ae427a2c377bbe9f
nelsontky <nelson@ccb.wtf>
2023-01-15 15:48:17 +0800

    feat: update program to support multi clicks

--- packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/constants.rs
+++ packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/constants.rs
@@ -2,3 +2,4 @@ pub const VOTE_SECONDS: i64 = 10;
 pub const V2_NUMBER_OF_BUTTONS: usize = 13;
 pub const NUMBER_OF_BUTTONS: usize = 15;
 pub const MAX_BUTTONS_PER_ROUND: usize = 10;
+pub const MAX_BUTTON_PRESS_COUNT: u8 = 2;
--- packages/solana-plays-pokemon-program/target/types/solana_plays_pokemon_program.ts
+++ packages/solana-plays-pokemon-program/target/types/solana_plays_pokemon_program.ts
@@ -80,463 +80,9 @@ export type SolanaPlaysPokemonProgram = {
         {
           "name": "joypadButton",
           "type": "u8"
-        }
-      ]
-    },
-    {
-      "name": "updateGameState",
-      "accounts": [
-        {
-          "name": "gameState",
-          "isMut": true,
-          "isSigner": false
         },
         {
-          "name": "nextGameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "gameData",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "framesImageCid",
-          "type": "string"
-        },
-        {
-          "name": "saveStateCid",
-          "type": "string"
-        }
-      ]
-    },
-    {
-      "name": "migrateGameStateToV4",
-      "accounts": [
-        {
-          "name": "gameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "nextGameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "nextNextGameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "gameData",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "framesImageCid",
-          "type": "string"
-        },
-        {
-          "name": "saveStateCid",
-          "type": "string"
-        }
-      ]
-    }
-  ],
-  "accounts": [
-    {
-      "name": "gameData",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "executedStatesCount",
-            "type": "u32"
-          },
-          {
-            "name": "isExecuting",
-            "type": "bool"
-          },
-          {
-            "name": "authority",
-            "type": "publicKey"
-          }
-        ]
-      }
-    },
-    {
-      "name": "gameStateV4",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "version",
-            "type": "u8"
-          },
-          {
-            "name": "index",
-            "type": "u32"
-          },
-          {
-            "name": "buttonPresses",
-            "type": "bytes"
-          },
-          {
-            "name": "createdAt",
-            "type": "i64"
-          },
-          {
-            "name": "framesImageCid",
-            "type": "string"
-          },
-          {
-            "name": "saveStateCid",
-            "type": "string"
-          }
-        ]
-      }
-    },
-    {
-      "name": "gameStateV3",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "index",
-            "type": "u32"
-          },
-          {
-            "name": "votes",
-            "type": {
-              "array": [
-                "u8",
-                15
-              ]
-            }
-          },
-          {
-            "name": "createdAt",
-            "type": "i64"
-          },
-          {
-            "name": "executedButton",
-            "type": "i8"
-          },
-          {
-            "name": "framesImageCid",
-            "type": "string"
-          },
-          {
-            "name": "saveStateCid",
-            "type": "string"
-          }
-        ]
-      }
-    },
-    {
-      "name": "gameStateV2",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "index",
-            "type": "u32"
-          },
-          {
-            "name": "votes",
-            "type": {
-              "array": [
-                "u8",
-                13
-              ]
-            }
-          },
-          {
-            "name": "createdAt",
-            "type": "i64"
-          },
-          {
-            "name": "executedButton",
-            "type": "i8"
-          },
-          {
-            "name": "framesImageCid",
-            "type": "string"
-          },
-          {
-            "name": "saveStateCid",
-            "type": "string"
-          }
-        ]
-      }
-    },
-    {
-      "name": "gameState",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "index",
-            "type": "u32"
-          },
-          {
-            "name": "upCount",
-            "type": "u32"
-          },
-          {
-            "name": "downCount",
-            "type": "u32"
-          },
-          {
-            "name": "leftCount",
-            "type": "u32"
-          },
-          {
-            "name": "rightCount",
-            "type": "u32"
-          },
-          {
-            "name": "aCount",
-            "type": "u32"
-          },
-          {
-            "name": "bCount",
-            "type": "u32"
-          },
-          {
-            "name": "startCount",
-            "type": "u32"
-          },
-          {
-            "name": "selectCount",
-            "type": "u32"
-          },
-          {
-            "name": "nothingCount",
-            "type": "u32"
-          },
-          {
-            "name": "createdAt",
-            "type": "i64"
-          },
-          {
-            "name": "executedButton",
-            "type": {
-              "defined": "JoypadButton"
-            }
-          },
-          {
-            "name": "framesImageCid",
-            "type": "string"
-          },
-          {
-            "name": "saveStateCid",
-            "type": "string"
-          }
-        ]
-      }
-    }
-  ],
-  "types": [
-    {
-      "name": "JoypadButton",
-      "type": {
-        "kind": "enum",
-        "variants": [
-          {
-            "name": "Up"
-          },
-          {
-            "name": "Down"
-          },
-          {
-            "name": "Left"
-          },
-          {
-            "name": "Right"
-          },
-          {
-            "name": "A"
-          },
-          {
-            "name": "B"
-          },
-          {
-            "name": "Start"
-          },
-          {
-            "name": "Select"
-          },
-          {
-            "name": "Nothing"
-          }
-        ]
-      }
-    }
-  ],
-  "events": [
-    {
-      "name": "ExecuteGameState",
-      "fields": [
-        {
-          "name": "buttonPresses",
-          "type": {
-            "array": [
-              "u8",
-              10
-            ]
-          },
-          "index": false
-        },
-        {
-          "name": "index",
-          "type": "u32",
-          "index": false
-        },
-        {
-          "name": "gameDataId",
-          "type": "publicKey",
-          "index": false
-        }
-      ]
-    }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "GameIsExecuting",
-      "msg": "Button presses are not allowed when the game is executing."
-    },
-    {
-      "code": 6001,
-      "name": "NoUpdatesIfNotExecuting",
-      "msg": "Game state cannot be updated when the game is not executing."
-    },
-    {
-      "code": 6002,
-      "name": "InvalidButton",
-      "msg": "Invalid button sent."
-    }
-  ]
-};
-
-export const IDL: SolanaPlaysPokemonProgram = {
-  "version": "0.1.0",
-  "name": "solana_plays_pokemon_program",
-  "instructions": [
-    {
-      "name": "initialize",
-      "accounts": [
-        {
-          "name": "gameData",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "gameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "nextGameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "framesImageCid",
-          "type": "string"
-        },
-        {
-          "name": "saveStateCid",
-          "type": "string"
-        }
-      ]
-    },
-    {
-      "name": "sendButton",
-      "accounts": [
-        {
-          "name": "gameState",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "gameData",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "player",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "clock",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
-        {
-          "name": "joypadButton",
+          "name": "pressCount",
           "type": "u8"
         }
       ]
@@ -910,6 +456,478 @@ export const IDL: SolanaPlaysPokemonProgram = {
       "code": 6002,
       "name": "InvalidButton",
       "msg": "Invalid button sent."
+    },
+    {
+      "code": 6003,
+      "name": "InvalidButtonPressCount",
+      "msg": "Invalid button press count."
+    }
+  ]
+};
+
+export const IDL: SolanaPlaysPokemonProgram = {
+  "version": "0.1.0",
+  "name": "solana_plays_pokemon_program",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "gameData",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "gameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "nextGameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "clock",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "framesImageCid",
+          "type": "string"
+        },
+        {
+          "name": "saveStateCid",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "sendButton",
+      "accounts": [
+        {
+          "name": "gameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "gameData",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "player",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "clock",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "joypadButton",
+          "type": "u8"
+        },
+        {
+          "name": "pressCount",
+          "type": "u8"
+        }
+      ]
+    },
+    {
+      "name": "updateGameState",
+      "accounts": [
+        {
+          "name": "gameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "nextGameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "gameData",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "clock",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "framesImageCid",
+          "type": "string"
+        },
+        {
+          "name": "saveStateCid",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "migrateGameStateToV4",
+      "accounts": [
+        {
+          "name": "gameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "nextGameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "nextNextGameState",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "gameData",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "clock",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "framesImageCid",
+          "type": "string"
+        },
+        {
+          "name": "saveStateCid",
+          "type": "string"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "gameData",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "executedStatesCount",
+            "type": "u32"
+          },
+          {
+            "name": "isExecuting",
+            "type": "bool"
+          },
+          {
+            "name": "authority",
+            "type": "publicKey"
+          }
+        ]
+      }
+    },
+    {
+      "name": "gameStateV4",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "version",
+            "type": "u8"
+          },
+          {
+            "name": "index",
+            "type": "u32"
+          },
+          {
+            "name": "buttonPresses",
+            "type": "bytes"
+          },
+          {
+            "name": "createdAt",
+            "type": "i64"
+          },
+          {
+            "name": "framesImageCid",
+            "type": "string"
+          },
+          {
+            "name": "saveStateCid",
+            "type": "string"
+          }
+        ]
+      }
+    },
+    {
+      "name": "gameStateV3",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "index",
+            "type": "u32"
+          },
+          {
+            "name": "votes",
+            "type": {
+              "array": [
+                "u8",
+                15
+              ]
+            }
+          },
+          {
+            "name": "createdAt",
+            "type": "i64"
+          },
+          {
+            "name": "executedButton",
+            "type": "i8"
+          },
+          {
+            "name": "framesImageCid",
+            "type": "string"
+          },
+          {
+            "name": "saveStateCid",
+            "type": "string"
+          }
+        ]
+      }
+    },
+    {
+      "name": "gameStateV2",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "index",
+            "type": "u32"
+          },
+          {
+            "name": "votes",
+            "type": {
+              "array": [
+                "u8",
+                13
+              ]
+            }
+          },
+          {
+            "name": "createdAt",
+            "type": "i64"
+          },
+          {
+            "name": "executedButton",
+            "type": "i8"
+          },
+          {
+            "name": "framesImageCid",
+            "type": "string"
+          },
+          {
+            "name": "saveStateCid",
+            "type": "string"
+          }
+        ]
+      }
+    },
+    {
+      "name": "gameState",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "index",
+            "type": "u32"
+          },
+          {
+            "name": "upCount",
+            "type": "u32"
+          },
+          {
+            "name": "downCount",
+            "type": "u32"
+          },
+          {
+            "name": "leftCount",
+            "type": "u32"
+          },
+          {
+            "name": "rightCount",
+            "type": "u32"
+          },
+          {
+            "name": "aCount",
+            "type": "u32"
+          },
+          {
+            "name": "bCount",
+            "type": "u32"
+          },
+          {
+            "name": "startCount",
+            "type": "u32"
+          },
+          {
+            "name": "selectCount",
+            "type": "u32"
+          },
+          {
+            "name": "nothingCount",
+            "type": "u32"
+          },
+          {
+            "name": "createdAt",
+            "type": "i64"
+          },
+          {
+            "name": "executedButton",
+            "type": {
+              "defined": "JoypadButton"
+            }
+          },
+          {
+            "name": "framesImageCid",
+            "type": "string"
+          },
+          {
+            "name": "saveStateCid",
+            "type": "string"
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "JoypadButton",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "Up"
+          },
+          {
+            "name": "Down"
+          },
+          {
+            "name": "Left"
+          },
+          {
+            "name": "Right"
+          },
+          {
+            "name": "A"
+          },
+          {
+            "name": "B"
+          },
+          {
+            "name": "Start"
+          },
+          {
+            "name": "Select"
+          },
+          {
+            "name": "Nothing"
+          }
+        ]
+      }
+    }
+  ],
+  "events": [
+    {
+      "name": "ExecuteGameState",
+      "fields": [
+        {
+          "name": "buttonPresses",
+          "type": {
+            "array": [
+              "u8",
+              10
+            ]
+          },
+          "index": false
+        },
+        {
+          "name": "index",
+          "type": "u32",
+          "index": false
+        },
+        {
+          "name": "gameDataId",
+          "type": "publicKey",
+          "index": false
+        }
+      ]
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "GameIsExecuting",
+      "msg": "Button presses are not allowed when the game is executing."
+    },
+    {
+      "code": 6001,
+      "name": "NoUpdatesIfNotExecuting",
+      "msg": "Game state cannot be updated when the game is not executing."
+    },
+    {
+      "code": 6002,
+      "name": "InvalidButton",
+      "msg": "Invalid button sent."
+    },
+    {
+      "code": 6003,
+      "name": "InvalidButtonPressCount",
+      "msg": "Invalid button press count."
     }
   ]
 };
