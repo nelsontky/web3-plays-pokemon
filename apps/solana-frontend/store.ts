d32efc622259fcec3f89655cdb630eeec6bc5eb2
nelsontky <nelson@ccb.wtf>
2023-01-16 17:02:15 +0800

    feat: add history fetch slice

--- apps/solana-frontend/store.ts
+++ apps/solana-frontend/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import gameDataReducer from "./slices/gameDataSlice";
 import gameStatesReducer from "./slices/gameStatesSlice";
+import historyReducer from "./slices/historySlice";
 
 export const store = configureStore({
   reducer: {
     gameData: gameDataReducer,
     gameStates: gameStatesReducer,
+    history: historyReducer,
   },
 });
 
