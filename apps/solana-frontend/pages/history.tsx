ef291a80156709652dea4e798a3ae97f001dcf5a
nelsontky <nelson@ccb.wtf>
2023-01-16 18:37:40 +0800

    feat: add metadata to history page

--- apps/solana-frontend/pages/history.tsx
+++ apps/solana-frontend/pages/history.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import tw from "twin.macro";
 import HistoryExplorer from "../components/HistoryExplorer";
 import { useAppSelector } from "../hooks/redux";
@@ -26,11 +27,37 @@ const styles = {
   `,
 };
 
+const DESCRIPTION =
+  "Throwback to the moves made during this collaborative game of Pokemon on the Solana blockchain!";
+
 export default function History() {
   const gameDataStatus = useAppSelector((state) => state.gameData.status);
 
   return (
     <div css={styles.root}>
+      <Head>
+        <title>History</title>
+        <meta name="description" content={DESCRIPTION} key="desc" />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content="/og-image.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
       {gameDataStatus !== "succeeded" ? (
         <div css={styles.loadingContainer}>
           <p css={styles.loadingText}>Loading...</p>
