ed9f93f6a38cdec3054c65f12cee654442587b38
nelsontky <nelson@ccb.wtf>
2023-01-18 22:56:07 +0800

    feat: add logging and emit participants

--- packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/constants.rs
+++ packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/constants.rs
@@ -3,3 +3,20 @@ pub const V2_NUMBER_OF_BUTTONS: usize = 13;
 pub const NUMBER_OF_BUTTONS: usize = 15;
 pub const MAX_BUTTONS_PER_ROUND: usize = 10;
 pub const MAX_BUTTON_PRESS_COUNT: u8 = 2;
+pub const BUTTON_MAPPINGS: [&str; 15] = [
+    "DO NOTHING",
+    "UP",
+    "DOWN",
+    "LEFT",
+    "RIGHT",
+    "TURBO UP",
+    "TURBO DOWN",
+    "TURBO LEFT",
+    "TURBO RIGHT",
+    "A",
+    "B",
+    "START",
+    "SELECT",
+    "Turbo A",
+    "Turbo B",
+];
