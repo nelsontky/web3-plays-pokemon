08685028c53aca3b6291a469fe24240eced4915c
nelsontky <nelson@ccb.wtf>
2023-01-26 06:11:52 +0800

    refactor: revert to basic example config

--- apps/solana-frontend/next.config.js
+++ apps/solana-frontend/next.config.js
@@ -4,11 +4,4 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
