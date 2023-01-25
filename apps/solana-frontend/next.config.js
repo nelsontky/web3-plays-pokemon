cc70f884826acb07c608e166755eb48d7116795e
nelsontky <nelson@ccb.wtf>
2023-01-26 06:06:46 +0800

    feat: ignore ts errors

--- apps/solana-frontend/next.config.js
+++ apps/solana-frontend/next.config.js
@@ -4,4 +4,11 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
