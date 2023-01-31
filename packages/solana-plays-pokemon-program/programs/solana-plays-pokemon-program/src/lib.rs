90595190f4c68b8b92938a94ed9571299502e830
nelsontky <nelson@ccb.wtf>
2023-01-31 17:04:45 +0800

    feat: add spl support for nft minting

--- packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/lib.rs
+++ packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/lib.rs
@@ -5,12 +5,7 @@ use crate::errors::*;
 use crate::utils::*;
 
 use anchor_lang::prelude::*;
-use anchor_spl::metadata::{
-    create_master_edition_v3, create_metadata_accounts_v3, verify_sized_collection_item,
-    CreateMasterEditionV3, CreateMetadataAccountsV3, VerifySizedCollectionItem,
-};
-use anchor_spl::token::{mint_to, transfer, MintTo};
-use mpl_token_metadata::state::{Collection, Creator, DataV2};
+use anchor_spl::token::transfer;
 
 pub mod account;
 pub mod constants;
@@ -47,45 +42,67 @@ pub mod solana_plays_pokemon_program {
 
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<Initialize>,
-        frames_image_cid: String,
-        save_state_cid: String,
-    ) -> Result<()> {
-        let game_data = &mut ctx.accounts.game_data;
-        game_data.executed_states_count = 1;
-        game_data.authority = *ctx.accounts.authority.key;
-        game_data.nfts_minted = 0;
+    //// comment out all initialization code to keep program size small enough for deployment :')
+    // pub fn initialize(
+    //     ctx: Context<Initialize>,
+    //     frames_image_cid: String,
+    //     save_state_cid: String,
+    // ) -> Result<()> {
+    //     let game_data = &mut ctx.accounts.game_data;
+    //     game_data.executed_states_count = 1;
+    //     game_data.authority = *ctx.accounts.authority.key;
+    //     game_data.nfts_minted = 0;
 
-        msg!("Game data account initialized");
+    //     msg!("Game data account initialized");
 
-        let game_state = &mut ctx.accounts.game_state;
-        init_game_state(
-            game_state,
-            0,
-            ctx.accounts.clock.unix_timestamp,
-            &frames_image_cid,
-            &save_state_cid,
-        );
-        msg!("First game state account initialized");
+    //     let game_state = &mut ctx.accounts.game_state;
+    //     init_game_state(
+    //         game_state,
+    //         0,
+    //         ctx.accounts.clock.unix_timestamp,
+    //         &frames_image_cid,
+    //         &save_state_cid,
+    //     );
+    //     msg!("First game state account initialized");
 
-        // init next game state
-        let next_game_state = &mut ctx.accounts.next_game_state;
-        init_game_state(
-            next_game_state,
-            game_data.executed_states_count,
-            ctx.accounts.clock.unix_timestamp,
-            &String::from(""),
-            &String::from(""),
-        );
-        msg!("Second game state account initialized");
+    //     // init next game state
+    //     let next_game_state = &mut ctx.accounts.next_game_state;
+    //     init_game_state(
+    //         next_game_state,
+    //         game_data.executed_states_count,
+    //         ctx.accounts.clock.unix_timestamp,
+    //         &String::from(""),
+    //         &String::from(""),
+    //     );
+    //     msg!("Second game state account initialized");
 
-        // init current_participants
-        let current_participants = &mut ctx.accounts.current_participants;
-        current_participants.participants = Vec::new();
+    //     // init current_participants
+    //     let current_participants = &mut ctx.accounts.current_participants;
+    //     current_participants.participants = Vec::new();
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
+
+    // pub fn initialize_current_participants(
+    //     ctx: Context<InitializeCurrentParticipants>,
+    // ) -> Result<()> {
+    //     let current_participants = &mut ctx.accounts.current_participants;
+    //     current_participants.participants = Vec::new();
+
+    //     Ok(())
+    // }
+
+    // pub fn initialize_spl_prices(
+    //     ctx: Context<InitializePrices>,
+    //     _gas_mint: Pubkey,
+    //     amount_for_one_lamport: u64,
+    // ) -> Result<()> {
+    //     let spl_prices = &mut ctx.accounts.spl_prices;
+    //     spl_prices
+    //         .non_human_readable_prices
+    //         .push(amount_for_one_lamport);
+    //     Ok(())
+    // }
 
     pub fn send_button(ctx: Context<SendButton>, joypad_button: u8, press_count: u8) -> Result<()> {
         process_button_send(
@@ -105,19 +122,11 @@ pub mod solana_plays_pokemon_program {
         joypad_button: u8,
         press_count: u8,
     ) -> Result<()> {
-        // transfer gas fee
-        let spl_prices = &ctx.accounts.spl_prices;
-        let mut gas_transfer_amount: u64 = 0;
-        for price in spl_prices.non_human_readable_prices.iter() {
-            gas_transfer_amount += price;
-        }
-        gas_transfer_amount = ((gas_transfer_amount
-            / (spl_prices.non_human_readable_prices.len() as u64))
-            + u64::from(
-                gas_transfer_amount % (spl_prices.non_human_readable_prices.len() as u64) == 0, // round up
-            ))
-            * SEND_BUTTON_SPL_GAS_TRANSACTION_FEE_LAMPORTS;
-
+        // transfer spl gas
+        let spl_gas_amount = calculate_spl_gas_amount(
+            &ctx.accounts.spl_prices,
+            SEND_BUTTON_SPL_GAS_TRANSACTION_FEE_LAMPORTS,
+        );
         transfer(
             CpiContext::new(
                 ctx.accounts.token_program.to_account_info(),
@@ -127,7 +136,7 @@ pub mod solana_plays_pokemon_program {
                     to: ctx.accounts.gas_deposit_token_account.to_account_info(),
                 },
             ),
-            gas_transfer_amount,
+            spl_gas_amount,
         )?;
 
         process_button_send(
@@ -174,127 +183,76 @@ pub mod solana_plays_pokemon_program {
 
         Ok(())
     }
-
-    pub fn initialize_current_participants(
-        ctx: Context<InitializeCurrentParticipants>,
-    ) -> Result<()> {
-        let current_participants = &mut ctx.accounts.current_participants;
-        current_participants.participants = Vec::new();
-
-        Ok(())
-    }
-
     pub fn mint_frames_nft(
         ctx: Context<MintFramesNft>,
         _game_state_index: u32,
         name: String,
         metadata_uri: String,
     ) -> Result<()> {
-        // create mint and token account before minting 1 token to the user
-        mint_to(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                MintTo {
-                    mint: ctx.accounts.mint.to_account_info(),
-                    to: ctx.accounts.token_account.to_account_info(),
-                    authority: ctx.accounts.authority.to_account_info(),
-                },
-            ),
-            1,
-        )?;
-
-        // create metadata account
-        create_metadata_accounts_v3(
-            CpiContext::new(
-                ctx.accounts.token_metadata_program.to_account_info(),
-                CreateMetadataAccountsV3 {
-                    metadata: ctx.accounts.token_metadata_account.to_account_info(),
-                    mint: ctx.accounts.mint.to_account_info(),
-                    mint_authority: ctx.accounts.authority.to_account_info(),
-                    payer: ctx.accounts.user.to_account_info(),
-                    rent: ctx.accounts.rent.to_account_info(),
-                    system_program: ctx.accounts.system_program.to_account_info(),
-                    update_authority: ctx.accounts.authority.to_account_info(),
-                },
-            ),
-            DataV2 {
-                collection: Some(Collection {
-                    verified: false,
-                    key: ctx.accounts.collection_mint.key(),
-                }),
-                creators: Some(vec![Creator {
-                    address: ctx.accounts.authority.key(),
-                    share: 100,
-                    verified: true,
-                }]),
-                name,
-                symbol: "PKM".to_string(),
-                uri: metadata_uri,
-                seller_fee_basis_points: 0,
-                uses: None,
-            },
-            true,
-            true,
-            None,
-        )?;
-
-        // verify collection
-        verify_sized_collection_item(
-            CpiContext::new(
-                ctx.accounts.token_metadata_program.to_account_info(),
-                VerifySizedCollectionItem {
-                    collection_authority: ctx.accounts.authority.to_account_info(),
-                    collection_master_edition: ctx
-                        .accounts
-                        .collection_master_edition
-                        .to_account_info(),
-                    collection_metadata: ctx.accounts.collection_metadata.to_account_info(),
-                    collection_mint: ctx.accounts.collection_mint.to_account_info(),
-                    metadata: ctx.accounts.token_metadata_account.to_account_info(),
-                    payer: ctx.accounts.user.to_account_info(),
-                },
-            ),
-            None,
-        )?;
-
-        // create master edition account (prevents further minting)
-        create_master_edition_v3(
-            CpiContext::new(
-                ctx.accounts.token_metadata_program.to_account_info(),
-                CreateMasterEditionV3 {
-                    edition: ctx.accounts.master_edition.to_account_info(),
-                    metadata: ctx.accounts.token_metadata_account.to_account_info(),
-                    mint: ctx.accounts.mint.to_account_info(),
-                    mint_authority: ctx.accounts.authority.to_account_info(),
-                    payer: ctx.accounts.user.to_account_info(),
-                    rent: ctx.accounts.rent.to_account_info(),
-                    system_program: ctx.accounts.system_program.to_account_info(),
-                    token_program: ctx.accounts.token_program.to_account_info(),
-                    update_authority: ctx.accounts.authority.to_account_info(),
-                },
-            ),
-            None,
-        )?;
-
-        // add to on chain mint list
-        let game_data = &mut ctx.accounts.game_data;
-        game_data.nfts_minted = game_data.nfts_minted.checked_add(1).unwrap();
-        let minted_nft = &mut ctx.accounts.minted_nft;
-        minted_nft.mint = ctx.accounts.mint.key();
-
-        Ok(())
+        process_mint_frames_nft(
+            name,
+            metadata_uri,
+            &ctx.accounts.token_program,
+            &ctx.accounts.mint,
+            &ctx.accounts.token_account,
+            &ctx.accounts.authority,
+            &ctx.accounts.token_metadata_account,
+            &ctx.accounts.user,
+            &ctx.accounts.rent,
+            &ctx.accounts.system_program,
+            &ctx.accounts.collection_mint,
+            &ctx.accounts.collection_master_edition,
+            &ctx.accounts.token_metadata_program,
+            &ctx.accounts.collection_metadata,
+            &ctx.accounts.master_edition,
+            &mut ctx.accounts.game_data,
+            &mut ctx.accounts.minted_nft,
+        )
     }
 
-    pub fn initialize_spl_prices(
-        ctx: Context<InitializePrices>,
-        _gas_mint: Pubkey,
-        amount_for_one_lamport: u64,
+    pub fn mint_frames_nft_spl_gas(
+        ctx: Context<MintFramesNftSplGas>,
+        _game_state_index: u32,
+        name: String,
+        metadata_uri: String,
     ) -> Result<()> {
-        let spl_prices = &mut ctx.accounts.spl_prices;
-        spl_prices
-            .non_human_readable_prices
-            .push(amount_for_one_lamport);
-        Ok(())
+        // transfer spl gas
+        let spl_gas_amount = calculate_spl_gas_amount(
+            &ctx.accounts.spl_prices,
+            SEND_BUTTON_SPL_GAS_TRANSACTION_FEE_LAMPORTS,
+        );
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    authority: ctx.accounts.user.to_account_info(),
+                    from: ctx.accounts.gas_source_token_account.to_account_info(),
+                    to: ctx.accounts.gas_deposit_token_account.to_account_info(),
+                },
+            ),
+            spl_gas_amount,
+        )?;
+
+        process_mint_frames_nft(
+            name,
+            metadata_uri,
+            &ctx.accounts.token_program,
+            &ctx.accounts.mint,
+            &ctx.accounts.token_account,
+            &ctx.accounts.authority,
+            &ctx.accounts.token_metadata_account,
+            &ctx.accounts.user,
+            &ctx.accounts.rent,
+            &ctx.accounts.system_program,
+            &ctx.accounts.collection_mint,
+            &ctx.accounts.collection_master_edition,
+            &ctx.accounts.token_metadata_program,
+            &ctx.accounts.collection_metadata,
+            &ctx.accounts.master_edition,
+            &mut ctx.accounts.game_data,
+            &mut ctx.accounts.minted_nft,
+        )
     }
 
     pub fn update_spl_prices(
--- packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/utils.rs
+++ packages/solana-plays-pokemon-program/programs/solana-plays-pokemon-program/src/utils.rs
@@ -6,6 +6,21 @@ use crate::errors::*;
 use crate::ExecuteGameState;
 
 use anchor_lang::prelude::*;
+use anchor_spl::metadata::create_master_edition_v3;
+use anchor_spl::metadata::create_metadata_accounts_v3;
+use anchor_spl::metadata::verify_sized_collection_item;
+use anchor_spl::metadata::CreateMasterEditionV3;
+use anchor_spl::metadata::CreateMetadataAccountsV3;
+use anchor_spl::metadata::Metadata;
+use anchor_spl::metadata::VerifySizedCollectionItem;
+use anchor_spl::token::mint_to;
+use anchor_spl::token::Mint;
+use anchor_spl::token::MintTo;
+use anchor_spl::token::Token;
+use anchor_spl::token::TokenAccount;
+use mpl_token_metadata::state::Collection;
+use mpl_token_metadata::state::Creator;
+use mpl_token_metadata::state::DataV2;
 
 pub fn init_game_state(
     game_state: &mut Account<GameStateV4>,
@@ -102,4 +117,128 @@ pub fn process_button_send(
     }
 
     Ok(())
+}
+
+pub fn process_mint_frames_nft<'info>(
+    name: String,
+    metadata_uri: String,
+    token_program: &Program<'info, Token>,
+    mint: &Account<'info, Mint>,
+    token_account: &Account<'info, TokenAccount>,
+    authority: &Signer<'info>,
+    token_metadata_account: &UncheckedAccount<'info>,
+    user: &Signer<'info>,
+    rent: &Sysvar<'info, Rent>,
+    system_program: &Program<'info, System>,
+    collection_mint: &Account<'info, Mint>,
+    collection_master_edition: &UncheckedAccount<'info>,
+    token_metadata_program: &Program<'info, Metadata>,
+    collection_metadata: &UncheckedAccount<'info>,
+    master_edition: &UncheckedAccount<'info>,
+    game_data: &mut Account<'info, GameData>,
+    minted_nft: &mut Account<'info, MintedNft>,
+) -> Result<()> {
+    // create mint and token account before minting 1 token to the user
+    mint_to(
+        CpiContext::new(
+            token_program.to_account_info(),
+            MintTo {
+                mint: mint.to_account_info(),
+                to: token_account.to_account_info(),
+                authority: authority.to_account_info(),
+            },
+        ),
+        1,
+    )?;
+
+    // create metadata account
+    create_metadata_accounts_v3(
+        CpiContext::new(
+            token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                metadata: token_metadata_account.to_account_info(),
+                mint: mint.to_account_info(),
+                mint_authority: authority.to_account_info(),
+                payer: user.to_account_info(),
+                rent: rent.to_account_info(),
+                system_program: system_program.to_account_info(),
+                update_authority: authority.to_account_info(),
+            },
+        ),
+        DataV2 {
+            collection: Some(Collection {
+                verified: false,
+                key: collection_mint.key(),
+            }),
+            creators: Some(vec![Creator {
+                address: authority.key(),
+                share: 100,
+                verified: true,
+            }]),
+            name,
+            symbol: "PKM".to_string(),
+            uri: metadata_uri,
+            seller_fee_basis_points: 0,
+            uses: None,
+        },
+        true,
+        true,
+        None,
+    )?;
+
+    // // verify collection
+    verify_sized_collection_item(
+        CpiContext::new(
+            token_metadata_program.to_account_info(),
+            VerifySizedCollectionItem {
+                collection_authority: authority.to_account_info(),
+                collection_master_edition: collection_master_edition.to_account_info(),
+                collection_metadata: collection_metadata.to_account_info(),
+                collection_mint: collection_mint.to_account_info(),
+                metadata: token_metadata_account.to_account_info(),
+                payer: user.to_account_info(),
+            },
+        ),
+        None,
+    )?;
+
+    // create master edition account (prevents further minting)
+    create_master_edition_v3(
+        CpiContext::new(
+            token_metadata_program.to_account_info(),
+            CreateMasterEditionV3 {
+                edition: master_edition.to_account_info(),
+                metadata: token_metadata_account.to_account_info(),
+                mint: mint.to_account_info(),
+                mint_authority: authority.to_account_info(),
+                payer: user.to_account_info(),
+                rent: rent.to_account_info(),
+                system_program: system_program.to_account_info(),
+                token_program: token_program.to_account_info(),
+                update_authority: authority.to_account_info(),
+            },
+        ),
+        None,
+    )?;
+
+    // add to on chain mint list
+    game_data.nfts_minted = game_data.nfts_minted.checked_add(1).unwrap();
+    minted_nft.mint = mint.key();
+
+    Ok(())
+}
+
+pub fn calculate_spl_gas_amount(spl_prices: &Account<SplPrices>, number_of_lamports: u64) -> u64 {
+    let mut gas_transfer_amount: u64 = 0;
+    for price in spl_prices.non_human_readable_prices.iter() {
+        gas_transfer_amount += price;
+    }
+    gas_transfer_amount = ((gas_transfer_amount
+        / (spl_prices.non_human_readable_prices.len() as u64))
+        + u64::from(
+            gas_transfer_amount % (spl_prices.non_human_readable_prices.len() as u64) == 0, // round up
+        ))
+        * number_of_lamports;
+
+    gas_transfer_amount
 }
