e759a0de7dfdd2baaadaa3f2b34b9f5eb8472ed1
nelsontky <nelson@ccb.wtf>
2023-02-01 08:38:58 +0800

    chore: bootstrap xnft

---
+++ others/xnft/src/App.tsx
@@ -0,0 +1,80 @@
+import { registerRootComponent } from "expo";
+import { RecoilRoot } from "recoil";
+import { ActivityIndicator, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/dev";
+
+import { ExamplesScreens } from "./screens/ExamplesScreen";
+import { HomeScreen } from "./screens/HomeScreen";
+import { TokenListNavigator } from "./screens/TokenNavigator";
+
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="List"
+        component={TokenListNavigator}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Tokens",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bank" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Examples"
+        component={ExamplesScreens}
+        options={{
+          tabBarLabel: "Examples",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  return (
+    <RecoilRoot>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </RecoilRoot>
+  );
+}
+
+export default registerRootComponent(App);
---
+++ others/xnft/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import tw from "twrnc";
+
+export function HomeScreen() {
+  return (
+    <iframe
+      src="https://solana.playspokemon.xyz"
+      style={tw`border-0 w-full h-full`}
+    />
+  );
+}
